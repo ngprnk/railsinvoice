b08061584928d6fffdba4210b658e546321b510d
Nischal <nischal1024@gmail.com>
2015-05-11 14:20:20 +0800

    Rails invoice
    Invoice generating application using rails 4.

---
+++ app/views/details/demo.py
@@ -0,0 +1,15 @@
+class hi:
+
+	def __init__(self,id,name,classa,rollno):
+		self.id=id
+		self.name=name 
+		self.classa =classa 
+		self.rollno=rollno
+
+
+	def printev(self,params):
+		print params
+
+
+
+as=hi(1,"dfd",23,12)
